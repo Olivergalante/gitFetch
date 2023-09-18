6553a0ad1e700683cb579063064dd95b86feac69
Oliver Galante <oliverandcarli@gmail.com>
2023-09-18 16:17:39 -0400

    URL fixes to display

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,9 @@ async function fetchGit(apifetch) {
 	try {
 		const response = await fetch(apifetch);
 		const data = await response.json();
-		console.log(data);
+		const profileAvatar = data.avatar_url;
+		const followers = data.followers_url;
+		console.log(profileAvatar, followers);
 		return data;
 	} catch (error) {
 		console.error(error);
@@ -15,7 +17,8 @@ async function fetchGit(apifetch) {
 submitButton.addEventListener("click", function (event) {
 	event.preventDefault();
 
-	const gitHubName = document.querySelector("#gitHubName").value;
+	const gitHubNamespace = document.querySelector("#gitHubName").value;
+	const gitHubName = gitHubNamespace.split(" ").join("");
 	const apifetch = `https://api.github.com/users/${gitHubName}`;
 	console.log(apifetch);
 	fetchGit(apifetch);
